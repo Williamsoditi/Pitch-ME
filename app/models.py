994bf3298f73f98e40857399b6bc902a813d9e21
Williamsoditi <williams.oditi@student.moringaschool.com>
2022-05-09 00:31:51 +0300

    Fix : Fixes manage.py shell bug

--- app/models.py
+++ app/models.py
@@ -1,11 +1,11 @@
-from . import db, login_manager
+from . import db
 from datetime import datetime
 from werkzeug.security import generate_password_hash,check_password_hash
-from flask_login import UserMixin
+# from flask_login import UserMixin
 
-@login_manager.user_loader
-def load_user(user_id):
-    return User.query.get(int(user_id))
+# @login_manager.user_loader
+# def load_user(user_id):
+#     return User.query.get(int(user_id))
 
 class Pitch(db.Model):
     
@@ -53,7 +53,7 @@ class Comment(db.Model):
 
         return writer
 
-class User(UserMixin,db.Model):
+class User(db.Model):
     
     __tablename__ = 'users'
 
