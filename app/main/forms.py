e006938be1a8c6b6da79a2eefd769d2c875b5f28
Williamsoditi <williams.oditi@student.moringaschool.com>
2022-05-09 01:23:03 +0300

    Feat : Adds te forms to submit pitches to the database

---
+++ app/main/forms.py
@@ -0,0 +1,20 @@
+import imp
+from flask_wtf import FlaskForm
+from wtfforms import StringField,TextAreaField, SubmitField
+from wtforms.validators import InputRequired
+from wtforms.fields.choices import SelectField
+
+class UpdateProfile(FlaskForm):
+    bio = TextAreaField('Tell us about you.',validators = [InputRequired()])
+    submit = SubmitField('Submit')
+
+class CommentForm(FlaskForm):
+    title = StringField('Comment title')
+    comment = TextAreaField('Pitch comment')
+    submit = SubmitField('Submit')
+
+class PitchForm(FlaskForm):
+    category = SelectField('Add pitch category',choices = [('Pick-up Lines','Pick-up Lines'),('Sales','Sales'),('Innovation','Innovation'),('Humanity','Humanity'),('Music','Music'),('Tech','Tech')]) 
+    context = TextAreaField('Pitch itself')
+    submit = SubmitField('Submit')
+
