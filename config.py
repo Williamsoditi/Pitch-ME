7bb415278e75244c25685d4b11271edde84c51d4
Williamsoditi <williams.oditi@student.moringaschool.com>
2022-05-09 00:48:27 +0300

    Updates database schema and adds migrations

--- config.py
+++ config.py
@@ -4,8 +4,7 @@ class Config:
     '''
     General configuration of parent class
     '''
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://motinga:Access@localhost/pitch_ME'
-    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:Access@localhost/pitch_me'
 
 class ProdConfig(Config):
     '''
@@ -14,7 +13,7 @@ class ProdConfig(Config):
     Args:
         Config: The parent configuration class with General configuration settings
     '''
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    # SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
 
 
 #Add test config here
