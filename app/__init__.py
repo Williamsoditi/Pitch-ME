e3773f0105561272376cbf909db2183895406394
Williamsoditi <williams.oditi@student.moringaschool.com>
2022-05-08 09:17:47 +0300

    Feat : Adds database and configurations in the init file

---
+++ app/__init__.py
@@ -0,0 +1,23 @@
+from flask import Flask
+from config import config_options
+from flask_bootstrap import Bootstrap
+from flask_sqlalchemy import SQLAlchemy
+
+
+bootstrap = Bootstrap()
+db = SQLAlchemy()
+
+def create_app(config_name):
+    # Initializing app
+    app = Flask(__name__)
+
+    #Creating app configurations
+    app.config.from_object(config_options[config_name])
+
+    #Initializing flask extensions
+    bootstrap.init_app(app)
+    db.init_app(app)
+
+    #Registering the blueprint
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
--- config.py
+++ config.py
@@ -1,1 +1,37 @@
+import os
+
+class Config:
+    '''
+    General configuration of parent class
+    '''
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://motinga:Access@localhost/pitch_ME'
+    DEBUG = True
+
+class ProdConfig(Config):
+    '''
+    Production  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+
+
+#Add test config here
+
+class DevConfig(Config):
+    '''
+    Development  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+
+    DEBUG = True
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig,
+# 'test':TestConfig
+}
 
