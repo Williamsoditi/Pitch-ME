0b7ff09f6d14a69162efaa4797522a200519b61a
Williamsoditi <williams.oditi@student.moringaschool.com>
2022-05-09 20:24:14 +0300

    Feat: Adds tests and styling

--- app/auth/views.py
+++ app/auth/views.py
@@ -10,11 +10,12 @@ from ..email import mail_message
 def signup():
   form = SignupForm()
   if form.validate_on_submit():
-    user = User(email = form.email.data, username = form.name.data, password = form.password.data)
+    user = User(email = form.email.data, username = form.username.data, password = form.password.data)
     db.session.add(user)
     db.session.commit()
     mail_message("Welcome to Pitch- ME","email/welcome_user",user.email,user=user)
     return redirect(url_for('auth.login'))
+    title = 'New account'
   return render_template('auth/signup.html', signup_form = form)
 
 
--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
-export SECRET_KEY='qwerty12345'
-export MAIL_USERNAME='williams.oditi@student.moringaschool.com'
-export MAIL_PASSWORD='Williamsoditiolang'
+export SECRET_KEY=qwerty12345
+export MAIL_USERNAME=williams.oditi@student.moringaschool.com
+export MAIL_PASSWORD=Williamsoditiolang
 
 
 python3 manage.py server
