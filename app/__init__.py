c27958f05d0233bf3db0acb1d1e16d4ac261d175
Williamsoditi <williams.oditi@student.moringaschool.com>
2022-05-09 09:43:05 +0300

    Feat : Coonfigures images uploads

--- app/__init__.py
+++ app/__init__.py
@@ -3,10 +3,12 @@ from config import config_options
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from flask_uploads import UploadSet,configure_uploads,IMAGES
 
 
 bootstrap = Bootstrap()
 db = SQLAlchemy()
+photos = UploadSet ('photos',IMAGES)
 
 login_manager = LoginManager()
 login_manager.session_protection = 'strong'
@@ -25,6 +27,9 @@ def create_app(config_name):
     bootstrap.init_app(app)
     db.init_app(app)
     login_manager.init_app(app)
+
+    #Configure uploadset
+    configure_uploads(app,photos)
 
     #Registering the blueprint
     from .main import main as main_blueprint
