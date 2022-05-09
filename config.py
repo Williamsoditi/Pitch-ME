0582a088731c1f6a2bc64cd79688774a5821cbee
Williamsoditi <williams.oditi@student.moringaschool.com>
2022-05-09 13:49:21 +0300

    Feat: Code refactoring and adds email validations and email experience to the application

--- config.py
+++ config.py
@@ -6,7 +6,21 @@ class Config:
     '''
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:Access@localhost/pitch_me'
+
+    #  email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+
     UPLOADED_PHOTOS_DEST = 'app/static/photos'
+
+    #simplemdeconfigurations
+    SIMPLEMDE_JS_IIFE = True
+    SIMPLEMDE_USE_CDN = True
+
+    DEBUG = True
 
 class ProdConfig(Config):
     '''
