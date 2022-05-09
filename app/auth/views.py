74ebfd93032d75dd4bd0989db52594d5c30d8968
Williamsoditi <williams.oditi@student.moringaschool.com>
2022-05-10 02:41:04 +0300

    Fix : Refactors code to solve login bug

--- app/auth/views.py
+++ app/auth/views.py
@@ -25,7 +25,7 @@ def login():
   if signin_form.validate_on_submit():
     user = User.query.filter_by(email = signin_form.email.data).first()
     if user is not None and user.verify_password(signin_form.password.data):
-      login_user(user,signin_form.remember_me.data)
+      login_user(user,signin_form.remember.data)
       return redirect(request.args.get('next') or url_for('main.index'))
 
     flash('Invalid username or Password')
