5637c93b2699ed257fefc95c8a08dfc1928bfe75
Williamsoditi <williams.oditi@student.moringaschool.com>
2022-05-11 08:17:50 +0300

    Deployment struggles

--- config.py
+++ config.py
@@ -1,62 +1,13 @@
-# import os
-
-# from flask_sqlalchemy import SQLAlchemy
-
-# class Config:
-#     '''
-#     General configuration of parent class
-#     '''
-#     SECRET_KEY = os.environ.get('SECRET_KEY')
-#     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:Access@localhost/pitch_me'
-
-#     #  email configurations
-#     MAIL_SERVER = 'smtp.googlemail.com'
-#     MAIL_PORT = 587
-#     MAIL_USE_TLS = True
-#     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
-#     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-
-#     UPLOADED_PHOTOS_DEST = 'app/static/photos'
-
-#     #simplemdeconfigurations
-#     SIMPLEMDE_JS_IIFE = True
-#     SIMPLEMDE_USE_CDN = True
-
-#     DEBUG = True
-
-# class ProdConfig(Config):
-#     '''
-#     Production  configuration child class
-
-#     Args:
-#         Config: The parent configuration class with General configuration settings
-#     '''
-#     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
-
-# class TestConfig(Config):
-#     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:Access@localhost/pitch_me_test'
-
-# class DevConfig(Config):
-#     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:Access@localhost/pitch_me'
-
-#     DEBUG = True
-
-# config_options = {
-# 'development':DevConfig,
-# 'production':ProdConfig,
-# 'test':TestConfig
-# }
 import os
+
+from flask_sqlalchemy import SQLAlchemy
 
 class Config:
     '''
     General configuration of parent class
     '''
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL').replace("://", "ql://", 1)
-    SQLALCHEMY_TRACK_MODIFICATIONS=True
-
-    # SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:Access@localhost/pitch_me'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:Access@localhost/pitch_me'
 
     #  email configurations
     MAIL_SERVER = 'smtp.googlemail.com'
@@ -71,43 +22,26 @@ class Config:
     SIMPLEMDE_JS_IIFE = True
     SIMPLEMDE_USE_CDN = True
 
-    @staticmethod
-    def init_app(app):
-        pass
 
 class ProdConfig(Config):
     '''
     Production  configuration child class
+
     Args:
         Config: The parent configuration class with General configuration settings
     '''
-    
-
-SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL').replace("://", "ql://", 1)
-
-
-DEBUG = True
-
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
 
 class TestConfig(Config):
-    '''
-    Test
-    '''
-SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL').replace("://", "ql://", 1)
-
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:Access@localhost/pitch_me_test'
 
 class DevConfig(Config):
-    '''
-    Development  configuration child class
-    Args:
-        Config: The parent configuration class with General configuration settings
-    '''
-SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL').replace("://", "ql://", 1)
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:Access@localhost/pitch_me'
 
+    DEBUG = True
 
 config_options = {
 'development':DevConfig,
 'production':ProdConfig,
-'test':TestConfig,
+'test':TestConfig
 }
-
