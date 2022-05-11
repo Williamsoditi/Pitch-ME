8f377616327548b47ed5eea6f0ff2cc8d170a02a
Williamsoditi <williams.oditi@student.moringaschool.com>
2022-05-11 09:20:52 +0300

    Deployment to heroku

--- manage.py
+++ manage.py
@@ -1,6 +1,8 @@
-from app import create_app,db
 from flask_script import Manager,Server
 from  flask_migrate import Migrate, MigrateCommand
+
+from app import create_app,db
+
 from app.models import User,Pitch,Comment,Upvote,Downvote
 
 # Creating app instance
@@ -8,9 +10,6 @@ app = create_app('production')
 
 manager = Manager(app)
 manager.add_command('server',Server)
-
-migrate = Migrate(app,db)
-manager.add_command('db',MigrateCommand)
 
 @manager.command
 def test():
@@ -23,6 +22,8 @@ def test():
 def make_shell_context():
     return dict(app = app,db = db,User = User,Pitch = Pitch, Comment = Comment,Upvote = Upvote, Downvote = Downvote)
 
+migrate = Migrate(app,db)
+manager.add_command('db',MigrateCommand)
 
 if __name__ == '__main__':
     manager.run()
